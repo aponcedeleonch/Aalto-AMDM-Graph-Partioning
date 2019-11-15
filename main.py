2c3a664e8f220c495cb74b9f62c3334371142d69
Alejandro Ponce de Leon <aponcedeleonch@gmail.com>
2019-11-15 18:01:00 +0200

    read graph file and read lines

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from graph_filenames import graphs_files
 import sys
 import argparse
+import numpy as np
 
 
 # Parse script arguments
-def parse_args(args=sys.argv[1:]):
+def parse_args(graph_names, args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument("--graph", "-g", type=str, required=True,
-                        help="Graph to execute the algorithm")
+                        help="Graph to execute the algorithm",
+                        choices=graph_names),
     parser.add_argument("--verbose", "-v", help="Verbose output",
                         action="store_true")
     return parser.parse_args(args)
 
 
+def get_adj_matrix(graph_file):
+    lines = graph_file.split("\n")
+    print(lines[0])
+
+
 if __name__ == '__main__':
-    args = parse_args()
-    if args.verbose:
-        print(args.graph)
-    else:
-        print("no verbose")
+    args = parse_args(list(graphs_files.keys()))
+    graph_file_content = ''
+    with open(graphs_files[args.graph], 'r') as file:
+        graph_file_content = file.read()
+    get_adj_matrix(graph_file_content)
