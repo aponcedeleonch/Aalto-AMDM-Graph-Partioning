4378dfdc0e465cb68a8f1b945592e75c61b64ad1
Alejandro Ponce de Leon <aponcedeleonch@gmail.com>
2019-11-21 15:43:34 +0200

    renamed file to more appropiate naming

---
+++ resources.py
@@ -0,0 +1,14 @@
+graphs_files = {
+    'ca-AstroPh': 'graphs_processed/ca-AstroPh.txt',
+    'ca-CondMat': 'graphs_processed/ca-CondMat.txt',
+    'ca-GrQc': 'graphs_processed/ca-GrQc.txt',
+    'ca-HepPh': 'graphs_processed/ca-HepPh.txt',
+    'ca-HepTh': 'graphs_processed/ca-HepTh.txt',
+    'Oregon-1': 'graphs_processed/Oregon-1.txt',
+    'roadNet-CA': 'graphs_processed/roadNet-CA.txt',
+    'soc-Epinions1': 'graphs_processed/soc-Epinions1.txt',
+    'web-NotreDame': 'graphs_processed/web-NotreDame.txt',
+    'kosta': 'graphs_processed/kosta.txt'
+}
+
+algorithms = ["Unorm", 'NormLap', 'NormEig']
